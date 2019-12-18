001e5e905d685506cec3f229e7ac02b3bcdaa3c5
Leila <leilaapsilva26@gmail.com>
2019-12-18 11:27:48 -0300

    Muda query para obter notícias em português

--- js/script.js
+++ js/script.js
@@ -20,11 +20,14 @@ function loadData() {
 
     $greeting.text('Então você quer morar em ' + address + '?');
 
+
+    // -------------------------------- Google maps --------------------------
+
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    // NY Times AJAX request
+    // -------------------------------- NY Times ----------------------------- 
 
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=cGkDcrqi4RzqMJkx31FqYQeewkGcIgO2'
 
@@ -44,7 +47,7 @@ function loadData() {
     //  $nytHeaderElem.text('Os artigos não foram carregados pois ocorreu um erro');
     //}); 
 
-    // Wikipedia AJAX request
+    // ------------------------------- Wikipedia ------------------------------
 
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 
@@ -70,19 +73,20 @@ function loadData() {
         }
     });
 
+    // ----------------------------- News API -----------------------------------
 
     // News api: f3fdc4acb3764b64b9f8a373792e5b72
     // https://newsapi.org/v2/everything?q=s%C3%A3o%20carlos%20sp&apiKey=f3fdc4acb3764b64b9f8a373792e5b72
 
     // Notícias brasileiras sobre a cidade pesquisada
-    var newsUrl = 'https://newsapi.org/v2/everything?q=' + 'sao'+ '&apiKey=f3fdc4acb3764b64b9f8a373792e5b72'
+    var newsUrl = 'https://newsapi.org/v2/everything?q=' + cityStr+ '&language=pt&apiKey=f3fdc4acb3764b64b9f8a373792e5b72'
 
     $.getJSON(newsUrl, function (data) {
         $newsHeaderElem.text('Notícias sobre ' + cityStr);
         news = data.articles;
-        for (var i = 0; i < news.length; i++) {
+        for (var i = 0; i < 10; i++) {
             var newsObj = news[i];
-            $newsElem.append('<li class="news">' + '<a href="' + newsObj.web_url + '">' + newsObj.headline.main + '</a>' + '<p>' + newsObj.snippet + '</p>' + '</li>');
+            $newsElem.append('<li class="news">' + '<a href="' + newsObj.web_url + '">' + newsObj.title + '</a>' + '<p>' + newsObj.content + '</p>' + '</li>');
         };
     });
 
